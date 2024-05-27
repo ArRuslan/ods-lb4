6d513bac3ce59ce1111355736421fa40897e20c3
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-05-27 13:45:17 +0300

    still no idea

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from math import sqrt
+
 import numpy as np
 from matplotlib import pyplot as plt
 from sympy import symbols, diff, solve, Symbol
 
-ORDER = 4
 
-ORDER += 1
 W = np.array([
     (2.24, 0.07), (1.61, -1.85), (4.66, -1.9), (3.52, -0.42), (1.4, -2.51), (3.88, -2.45), (4.44, -3.12), (4.38, -2.54),
     (1.08, -1.95), (3.97, -2.01), (3.43, 0.08), (3.39, -0.91), (2.48, 0.25), (4.27, -2.75), (0.85, 1.33), (3.13, 1),
@@ -14,42 +14,86 @@ W = np.array([
 ])
 xs = W[:, 0]
 ys = W[:, 1]
-
 x = symbols("x", real=True)
-a_s = symbols(", ".join([f"a{i}" for i in range(ORDER)]), real=True)
-Dxb = 0
-for xi, yi in W:
-    eq = 0
-    for i in range(ORDER):
-        eq += a_s[i] * xi ** i
-    Dxb += (yi - eq) ** 2
 
-Dxb_s = [diff(Dxb, a) for a in a_s]
-solved = solve(Dxb_s, a_s, dict=True)[0]
 
-Px: Symbol = 0
-for i, a in enumerate(a_s):
-    Px += solved[a] * x ** i
+def find_polinomial_of_order(order: int) -> Symbol:
+    order += 1
 
-print(Px)
+    a_s = symbols(", ".join([f"a{i}" for i in range(order)]), real=True)
+    Dxb = 0
+    for xi, yi in W:
+        eq = 0
+        for i in range(order):
+            eq += a_s[i] * xi ** i
+        Dxb += (yi - eq) ** 2
 
-x_plot = np.linspace(xs.min(), xs.max())
-y_plot = [Px.evalf(subs={x: x_p}) for x_p in x_plot]
-plt.plot(x_plot, y_plot, color='#58b970', label='Regression Line')
+    Dxb_s = [diff(Dxb, a) for a in a_s]
+    solved = solve(Dxb_s, a_s, dict=True)[0]
 
-"""
-x_plot1 = np.linspace(xs.min(), xs.max())
-y_plot1 = [(Px + Px * 0.05).evalf(subs={x: x_p}) for x_p in x_plot]
-plt.plot(x_plot1, y_plot1, color='#0000ff', label='Idk')
+    Px: Symbol = 0
+    for i, a in enumerate(a_s):
+        Px += solved[a] * x ** i
 
-x_plot2 = np.linspace(xs.min(), xs.max())
-y_plot2 = [(Px - Px * 0.05).evalf(subs={x: x_p}) for x_p in x_plot]
-plt.plot(x_plot2, y_plot2, color='#0000ff', label='Idk')
-"""
+    return Px
 
-plt.scatter(xs, ys, c='#ef5423', label='Scatter Plot')
 
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.legend()
-plt.show()
+def find_ssq(Px: Symbol):
+    Ssq = sum([(yi - Px.evalf(subs={x: xi})) ** 2 for xi, yi in W])
+    Ssq /= len(W) - 2
+    Ssq = sqrt(Ssq)
+    Ssq *= 2
+
+    return Ssq
+
+
+def idk() -> None:
+    Pxl = find_polinomial_of_order(1)
+    sl = sum([(yi - Pxl.evalf(subs={x: xi})) ** 2 for xi, yi in W])
+    sl /= len(W) - 2
+
+    for i in range(2, 10):
+        Pxc = find_polinomial_of_order(i)
+        sc = sum([(yi - Pxc.evalf(subs={x: xi})) ** 2 for xi, yi in W])
+        sc /= len(W) - i - 1
+
+        s1 = max(sl, sc)
+        s2 = min(sl, sc)
+
+        Fn = s1 / s2
+        print(f"{i-1}/{i} : {Fn:.3f} ({sl:.3f}, {sc:.3f})")
+
+        Pxl, sl = Pxc, sc
+
+
+def main():
+    #idk()
+    #return
+
+    Px = find_polinomial_of_order(4)
+    print(Px)
+
+    #idk(Px, 4)
+
+    x_plot = np.linspace(xs.min(), xs.max())
+    y_plot = [Px.evalf(subs={x: x_p}) for x_p in x_plot]
+    plt.plot(x_plot, y_plot, color='#58b970', label='Лінія регресії')
+
+    Ssq = find_ssq(Px)
+
+    y_plot1 = [yp + Ssq for yp in y_plot]
+    plt.plot(x_plot, y_plot1, color='#0000ff', label='Довірча область', linestyle="dashed")
+
+    y_plot2 = [yp - Ssq for yp in y_plot]
+    plt.plot(x_plot, y_plot2, color='#0000ff', label='Довірча область', linestyle="dashed")
+
+    plt.scatter(xs, ys, c='#ef5423', label='Точки')
+
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
